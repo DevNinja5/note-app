6b0b66b53fc0b6a5a2b9f47b751c889e3d164244
Rahul Soni <rahul.soni@knoldus.com>
2021-12-20 03:34:40 +0530

    fixed creds

--- src/libs/createUser.js
+++ src/libs/createUser.js
@@ -1,13 +1,13 @@
 import User from "../models/User";
 
 export const createAdminUser = async () => {
-  const userFound = await User.findOne({ email: "admin" });
+  const userFound = await User.findOne({ email: "admin@local" });
 
   if (userFound) return;
 
   const newUser = new User({
     username: "admin",
-    email: "admin",
+    email: "admin@local",
   });
 
   newUser.password = await newUser.encryptPassword("admin");
